3e7176d2432314e45b7a9b3f443a6f1f54fd6f38
Luna18l <126378918+Luna18l@users.noreply.github.com>
2023-02-26 07:53:09 +0000

    Add files via upload

---
+++ loja.js
@@ -0,0 +1,62 @@
+function Produto(){
+    const produtos_cost = document.getElementById('produtos_cost');
+    produtos_cost.style.display = 'block';
+}
+function voltar(){
+    const produtos_cost = document.getElementById('produtos_cost');
+    produtos_cost.style.display = 'none';
+}
+
+function Produto3(){
+    const produtos_biju_soc = document.getElementById('produtos_biju_soc');
+    produtos_biju_soc.style.display = 'block';
+}
+
+function voltar3(){
+    const produtos_biju_soc = document.getElementById('produtos_biju_soc');
+    produtos_biju_soc.style.display = 'none';
+}
+
+function Produto2(){
+    const produtos_biju_sus = document.getElementById('produtos_biju_sus');
+    produtos_biju_sus.style.display = 'block';
+}
+
+function voltar2(){
+    const produtos_biju_sus = document.getElementById('produtos_biju_sus');
+    produtos_biju_sus.style.display = 'none';
+}
+
+
+function myFunction() {
+    alert("De momento, essa funcionalidade não está disponível.\n Visite as páginas dos artesãos para comprar os produtos.");
+  }
+
+function Mascote(){
+    const mascote_ang = document.getElementById('mascote_ang');
+    mascote_ang.style.display = 'block';
+}
+
+function Mascote1(){
+    const mascote_sus = document.getElementById('mascote_sus');
+    mascote_sus.style.display = 'block';
+}
+function Mascote2(){
+    const mascote_soc = document.getElementById('mascote_soc');
+    mascote_soc.style.display = 'block';
+}
+
+function voltar4(){
+    const mascote_ang = document.getElementById('mascote_ang');
+    mascote_ang.style.display = 'none';
+}
+
+function voltar5(){
+    const mascote_sus = document.getElementById('mascote_sus');
+    mascote_sus.style.display = 'none';
+}
+
+function voltar6(){
+    const mascote_soc = document.getElementById('mascote_soc');
+    mascote_soc.style.display = 'none';
+}
